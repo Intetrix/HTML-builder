715fe922250b9cf7c2e6ca07792a6b2eea4ed911
Tanya Sevko <taniasevko@yandex.ru>
2023-05-07 13:10:47 +0300

    feat: add solution to 04 task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,19 @@
+const path = require('path');
+const fs = require('fs');
 
+const filesReader = path.join(__dirname, "files");
+const copyReader = path.join(__dirname, "files-copy");
+
+fs.rm(copyReader, { recursive: true, force: true }, () => {
+    fs.readdir(filesReader, 
+      { withFileTypes: true },
+      (file, files) => {
+        fs.mkdir(copyReader, { recursive: true, force: true }, () => {
+          files.forEach(file => {
+            fs.copyFile(path.join(filesReader, file.name), path.join(copyReader, file.name), () => {
+            });
+          });
+        }); 
+      }
+    );
+});
